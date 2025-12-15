b61c3c6cfe0f64b6f06a671417435f8480308122
soopiri <sooyoon@kakao.com>
2025-12-15 23:03:41 +0900

    comeplete variable

--- variable.dart
+++ variable.dart
@@ -72,9 +72,66 @@ void finalVariable() {
 
 void lateVariable() {
   // late
-  // 초기 데이터 없이 변수를 선언할 수 있음 (클래스를 배워야 의미있는 키워드)
+  // 초기 데이터 없이 변수를 선언할 수 있음 (클래스를 배워야 의미있는 키워드라 할 수 있음)
   // late 없이 변수 선언 후 클래스 초기화 하면 에러 발생.
   // late가 있을 경우, 나중에 메서드에서 초기화 가능.
+  
+  late final String name;
+  // print(name); // error - late variable is not initialized yet.
+  
+  // do something, ex: go to api
+  
+  name = 'data from api';
+  print(name);
+}
+
+void constantVariable() {
+  // javascript, typescript의 const와는 다름 -> final과 비슷함
+  // dart에서는 compile-time constant를 만들어 줌. 
+  // compile 되는 시점에 해당 값이 결정되기 때문에, 런타임에 값이 변경될 수 없음. (ex. api 호출 결과 값 등)
+  const API_KEY = '1234567890';
+  print(API_KEY);
+}
+
+void recap() {
+  // dart stype guide에 따르면 함수 내에서는 var를 사용하는것을 권장.
+  // Class property는 타입을 명시적으로 선언하는것을 권장.
+  String a = 'a';
+  a='b';
+  var b = 'b';
+  b=1.toString();
+  final c = 'c'; 
+  print(a);
+  print(b);
+  print(c);
+
+  // dynamic type은 조심스럽게 사용해야 함 - 어떤 데이터가 들어오는지 모른다는 뜻
+  // var의 경우, 컴파일 시점에 타입이 고정되며, 이후 해당 변수는 해당 타입으로만 사용이 가능함.
+  // dynamic의 경우, 타입이 고정되지 않아 컴파일타임에도 오류가 나지 않음
+  // 정말로 타입이 여러 형태로 바뀌는 경우에만 조심스럽게 사용
+  // 특정 타입이 제공하는 함수를 사용하기 위해선 type check를 해야 함
+  dynamic d = 'd';
+  d = 1;
+  d = false;
+  print(d);
+
+  // const -> 컴파일 시점에 값이 결정됨. 런타임에 값이 변경될 수 없음. (ex. api 호출 결과 값 등)
+  const API_KEY = '1234567890';
+  print(API_KEY);
+
+  // null safety -> 변수가 null을 참조할 수 없도록 함
+  String name = 'soopiri';
+  // name = null; // error
+  print(name);
+
+  String? name2 = 'soopiri';
+  name2 = null; // ok
+  print(name2);
+
+  // late -> 초기 데이터 없이 변수를 선언할 수 있음 (클래스를 배워야 의미있는 키워드라 할 수 있음)
+  late String name3;
+  name3 = 'soopiri';
+  print(name3);
 }
 
 // main : Dart 프로그램의 Entry Point (필수)
@@ -87,5 +144,6 @@ void main() {
   // dynamicType();
   // nullSafety();
   // finalVariable();
-  lateVariable();
+  // lateVariable();
+   constantVariable();
 }
